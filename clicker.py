f70e61ad8baeb35c114338bc8345d875e634776c
GustavFlachs <152276245+GustavFlachs@users.noreply.github.com>
2025-01-09 20:38:33 +0100

    updt

--- clicker.py
+++ clicker.py
@@ -5,7 +5,7 @@ import threading
 multiplier = 1
 f_state = False
 cashmoney = int()
-price = 50
+price = 20
 upgrade = 0
 upgrade_prompt = 0
 
@@ -13,9 +13,9 @@ def money():
     print(f"You have:{cashmoney} $.")
 
 def wait_for_upgrade():
-    global upgrade_prompt
+    global upgrade_prompt #bez tohoto global by ze šedivěk upgrade_prompt na line 19 a přestal by fungovat
     print(f"+1 money gain available for {price}$ to buy press y.")
-    time.sleep(5)
+    time.sleep(2.5)
     upgrade_prompt = False
 
 print("Press F to gain money.")
@@ -28,11 +28,11 @@ while True:
     elif keyboard.is_pressed("f") == False:
         f_state = False
         if cashmoney >= price and upgrade_prompt == False:
-            threading.Thread(target=wait_for_upgrade).start()
+            threading.Thread(target=wait_for_upgrade).start() #pomocí tohoto jde klikat f a gainovat cash i když time.sleep(2.5) je aktivní (zapne ten def paralelně s hlavním programem)
             upgrade_prompt = True
     if keyboard.is_pressed("y") and cashmoney >= price:
         multiplier += 1
         cashmoney -= price
-        price += 50
+        price += 20
         money()
 #add an upgrade that makes passive money
